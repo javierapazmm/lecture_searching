424ae1a169ef81655dbdefcad43a61db470eb046
javierapazmm <jabelapaz@gmail.com>
2024-04-08 09:06:59 +0200

    .

--- searching.py
+++ searching.py
@@ -1,3 +1,5 @@
+#[]><{}
+import json
 import os
 
 # get current working directory path
@@ -12,10 +14,24 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    #lee el soubor
+    with open("sequential.json", "r") as f:
+        allowed_key = json.load(f)
+
+    #Funkce ověří, že zadaný klíč pochází z množiny povolených řešení. Pokud ne, funkce vrátí hodnotu None
+    if field not in allowed_key:
+        return None
+
+    with open(file_name, "r") as f:
+        data = json.load(f)
+
+    return data.get(field)
+
 
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
 
 
 if __name__ == '__main__':
